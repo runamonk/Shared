5c5860270df2ff320e1e13afee281a6086fca902
runamonk <runamonk@gmail.com>
2021-03-12 15:56:12 +0000

    Added checks to determine if the image should be resized, I don't want to size it up.

--- Utility.cs
+++ Utility.cs
@@ -299,15 +299,20 @@ namespace Utility
         #endregion
         public static Image ScaleImage(Image image, int maxWidth, int maxHeight)
         {
-            var ratioX = (double)maxWidth / image.Width;
-            var ratioY = (double)maxHeight / image.Height;
-            var ratio = Math.Min(ratioX, ratioY);
-            var newWidth = (int)(image.Width * ratio);
-            var newHeight = (int)(image.Height * ratio);
-            var newImage = new Bitmap(newWidth, newHeight);
-            using (var graphics = Graphics.FromImage(newImage))
-                graphics.DrawImage(image, 0, 0, newWidth, newHeight);
-            return newImage;
+            if ((image.Height > maxHeight) || (image.Width > maxWidth))
+            {
+                var ratioX = (double)maxWidth / image.Width;
+                var ratioY = (double)maxHeight / image.Height;
+                var ratio = Math.Min(ratioX, ratioY);
+                var newWidth = (int)(image.Width * ratio);
+                var newHeight = (int)(image.Height * ratio);
+                var newImage = new Bitmap(newWidth, newHeight);
+                using (var graphics = Graphics.FromImage(newImage))
+                    graphics.DrawImage(image, 0, 0, newWidth, newHeight);
+                return newImage;
+            }
+            else
+                return image;
         }
     }
 }
