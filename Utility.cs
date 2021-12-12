9e3df43aa3d24849bae30ecfe9b7d0215e0ea456
runamonk <runamonk@gmail.com>
2021-12-12 11:51:07 +0000

    updated ScaleImage() for better handling of undersized images.

--- Utility.cs
+++ Utility.cs
@@ -360,11 +360,18 @@ namespace Utility
                 var ratioX = (double)maxWidth / image.Width;
                 var ratioY = (double)maxHeight / image.Height;
                 var ratio = Math.Min(ratioX, ratioY);
-                var newWidth = (int)(image.Width * ratio);
-                var newHeight = (int)(image.Height * ratio);
+                var newWidth = image.Width;
+                var newHeight = image.Height;
+
+                if (image.Width > maxWidth)
+                    newWidth = (int)(image.Width * ratio);
+                if (image.Height > maxHeight)
+                    newHeight = (int)(image.Height * ratio);
+                
                 var newImage = new Bitmap(newWidth, newHeight);
                 using (var graphics = Graphics.FromImage(newImage))
                     graphics.DrawImage(image, 0, 0, newWidth, newHeight);
+
                 return newImage;
             }
             else
