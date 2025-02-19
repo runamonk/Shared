107e8145ae27fa425efbf5420b9dc11efa2f2f30
runamonk <ron.hines@outlook.com>
2025-02-19 15:24:44 +0000

    Cleanup

--- LowLevelHooks.cs
+++ LowLevelHooks.cs
@@ -10,62 +10,27 @@ namespace Utility
     public class LowLevelHooks
     {
         public delegate void OnKeyEventHandler(Keys key);
+        private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
 
-        private const int WhKeyboardLl = 13;
-        private const int WmKeydown = 0x0100;
-        private const int WmSyskeydown = 0x0104;
-        private const int VkLbutton = 0x01;
-        private const int VkRbutton = 0x02;
-        private const int VkMbutton = 0x04;
-        private readonly LowLevelKeyboardProc _proc;
+        private const int WH_KEYBOARD_LL = 13;
+        private const int WM_KEYDOWN = 0x0100;
+        private const int WM_SYSKEYDOWN = 0x0104;
+        private const int VK_LMBUTTON = 0x01;
+        private const int VK_RMBUTTON = 0x02;
+        private const int VK_MMBUTTON = 0x04;
+        
+        private readonly LowLevelKeyboardProc keyboardProc;
 
-        private bool _checkMouseButtons;
-        private IntPtr _keyboardHookId = IntPtr.Zero;
+        private bool doEventThread;
+        private IntPtr keyboardHookId = IntPtr.Zero;
 
-        private short _lButtonState;
-        private short _mButtonState;
-
-        private Thread _mouseThread;
-        private short _rButtonState;
-        private ThreadStart _start;
+        private Thread eventThread;
 
         public DateTime LastEventTime = DateTime.Now;
         public OnKeyEventHandler OnKeyPress;
-
-
-        public LowLevelHooks() { _proc = HookCallback; }
-
+        
         [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
         private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
-
-        private void CheckMouseButtons()
-        {
-            bool WasKeyChanged(ref short oldKeyState, int key)
-            {
-                short tmpState = GetKeyState(key);
-                if (tmpState != oldKeyState)
-                {
-                    oldKeyState = tmpState;
-                    SetLastEventTimeToNow();
-                    return true;
-                }
-
-                return false;
-            }
-
-            // set our defaults.
-            _lButtonState = GetKeyState(VkLbutton);
-
-            while (_checkMouseButtons)
-            {
-                Thread.Sleep(100);
-                if (WasKeyChanged(ref _lButtonState, VkLbutton))
-                    continue;
-                if (WasKeyChanged(ref _rButtonState, VkRbutton))
-                    continue;
-                WasKeyChanged(ref _mButtonState, VkMbutton);
-            }
-        }
 
         [DllImport("user32.dll")]
         public static extern short GetKeyState(int vKey);
@@ -73,15 +38,72 @@ namespace Utility
         [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
         private static extern IntPtr GetModuleHandle(string lpModuleName);
 
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        public LowLevelHooks() 
+        { 
+            keyboardProc = HookCallback; 
+        }
+
+        private void doEvents()
+        {
+            short lButtonState = GetKeyState(VK_LMBUTTON);
+            short rButtonState = GetKeyState(VK_RMBUTTON);
+            short mButtonState = GetKeyState(VK_MMBUTTON);
+            int cursorX = Cursor.Position.X;
+            int cursorY = Cursor.Position.Y;
+
+            bool WasKeyChanged(ref short oldKeyState, int key)
+            {
+                short tmpState = GetKeyState(key);
+                if (tmpState != oldKeyState)
+                {
+                    oldKeyState = tmpState;
+                    return true;
+                }
+
+                return false;
+            }
+
+            bool HasCursorMoved()
+            {
+                if (cursorX != Cursor.Position.X || cursorY != Cursor.Position.Y)
+                {
+                    cursorX = Cursor.Position.X;
+                    cursorY = Cursor.Position.Y;
+                    return true;
+                }
+                return false;
+            }
+
+            while (doEventThread)
+            {
+                Thread.Sleep(100);
+
+                if (WasKeyChanged(ref lButtonState, VK_LMBUTTON) || 
+                    WasKeyChanged(ref rButtonState, VK_RMBUTTON) || 
+                    WasKeyChanged(ref mButtonState, VK_MMBUTTON) || 
+                    HasCursorMoved())
+                   SetLastEventTimeToNow();
+
+                Application.DoEvents();
+            }
+        }
+
         private IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
         {
-            if ((nCode >= 0 && wParam == (IntPtr)WmKeydown) || wParam == (IntPtr)WmSyskeydown)
+            if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN || wParam == (IntPtr)WM_SYSKEYDOWN)
             {
                 OnKeyPress?.Invoke((Keys)Marshal.ReadInt32(lParam));
                 SetLastEventTimeToNow();
             }
 
-            return CallNextHookEx(_keyboardHookId, nCode, wParam, lParam);
+            return CallNextHookEx(keyboardHookId, nCode, wParam, lParam);
         }
 
         private IntPtr SetHook(LowLevelKeyboardProc proc, int wParam)
@@ -93,33 +115,27 @@ namespace Utility
             }
         }
 
-        public void SetLastEventTimeToNow() { LastEventTime = DateTime.Now; }
-
-        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-        private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+        public void SetLastEventTimeToNow() 
+        { 
+            LastEventTime = DateTime.Now; 
+        }
 
         public void Start()
         {
-            _keyboardHookId = SetHook(_proc, WhKeyboardLl);
-            _checkMouseButtons = true;
-            _start = CheckMouseButtons;
-            _mouseThread = new Thread(_start);
-            _mouseThread.Start();
+            keyboardHookId = SetHook(keyboardProc, WH_KEYBOARD_LL);
+            doEventThread = true;
+
+            eventThread = new Thread(new ThreadStart(doEvents));
+            eventThread.Start();
         }
 
         public void Stop()
         {
-            UnhookWindowsHookEx(_keyboardHookId);
+            UnhookWindowsHookEx(keyboardHookId);
 
-            _checkMouseButtons = false;
-            if (_mouseThread.ThreadState == ThreadState.Running)
-                _mouseThread.Abort();
+            doEventThread = false;
+            if (eventThread.ThreadState == ThreadState.Running)
+                eventThread.Abort();
         }
-
-        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
-
-        private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
     }
 }
